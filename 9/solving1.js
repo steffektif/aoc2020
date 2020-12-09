d1e50184dda74cb7d835985da82c3efe925bf042
Stefan Herz <stefan.herz@signavio.com>
2020-12-09 08:18:14 +0100

    solved 9.2

--- 9/solving1.js
+++ 9/solving1.js
@@ -10,7 +10,7 @@ var stepSize = 25;
 const findInPreamble = (preamble, target) => {
   for (let x = 0; x < preamble.length; x++) {
     for (let y = 0; y < preamble.length; y++) {
-      if (x != y) {
+      if (x !== y) {
         if (preamble[x] + preamble[y] == target) {
           return true;
         }
@@ -20,22 +20,48 @@ const findInPreamble = (preamble, target) => {
   return false;
 };
 
+const findContiguousBounds = (target) => {
+  for (let x = 0; x < numbers.length; x++) {
+    var sum = numbers[x];
+    for (let y = 1; y < numbers.length; y++) {
+      if (x < y) {
+        // console.log("sum", sum, "toAdd", numbers[y]);
+        sum = sum + numbers[y];
+        if (sum == target) {
+          return y > x ? { start: x, end: y + 1 } : { start: y, end: x };
+        }
+      }
+    }
+  }
+  return null;
+};
+
 lineReader.on("line", function (line) {
   numbers.push(parseFloat(line));
 });
 
 lineReader.on("close", () => {
-  var noFit = null;
+  var invalidNumber = null;
   var preamble = [];
   var currentIndex = 0;
 
-  while (noFit === null) {
+  while (invalidNumber === null) {
     preamble = numbers.slice(currentIndex, currentIndex + stepSize);
     const target = numbers[currentIndex + stepSize];
     if (!findInPreamble(preamble, target)) {
-      noFit = target;
+      invalidNumber = target;
     }
     currentIndex = currentIndex + 1;
   }
-  console.log(noFit);
+  console.log("invalid number: ", invalidNumber);
+
+  const bounds = findContiguousBounds(invalidNumber);
+
+  const contiguousRange = numbers.slice(bounds.start, bounds.end);
+
+  const encryptionWeakness =
+    Math.max.apply(null, contiguousRange) +
+    Math.min.apply(null, contiguousRange);
+
+  console.log("encryption weakness", encryptionWeakness);
 });
